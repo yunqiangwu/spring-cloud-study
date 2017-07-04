932151161b78391ae14a80e1e63ceb0161b2aeff
吴云强 <yunqiang.wu@hand-china.com>
2017-07-04 18:28:10 +0800

    first commit

---
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/study/EurekaApplication.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) HAND Enterprise Solutions Company Ltd.
+ * All Rights Reserved
+ */
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做服务发现.
+ * @author eacdy
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
